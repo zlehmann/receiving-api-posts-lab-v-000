b17d81fb88939e5c84150aad9f27b8d55ab52fe1
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 17:17:50 -0500

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static#home'
-  resources :orders
-  resources :invoices
-  resources :products, only: [:show, :index, :new, :create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
