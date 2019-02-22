935153a6378e9f509aa79a63e0dbcfe1d4984559
maxwellbenton <maxwellbenton@gmail.com>
2019-02-22 07:42:21 -0500

    add routes back in

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to: 'static#home'
+  resources :orders
+  resources :invoices
+  resources :products, only: [:show, :index, :new, :create]
 end
