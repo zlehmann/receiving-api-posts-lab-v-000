ca471a9a257e17bee1b9a4768c2da6ce51ffad39
Zachary Lehmann <zlehmann@gmail.com>
2019-04-20 12:47:25 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @product = Product.create(product_params)
+    render json: @product, status: 201
   end
 
   def show
